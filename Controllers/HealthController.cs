98057686af4a9ef442086087063645c5ddcc4695
yunqian44 <yunqian8@live.com>
2020-01-22 17:40:44 +0800

    add checkhealth

---
+++ Controllers/HealthController.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace k8s.net.demo.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class HealthController : ControllerBase
+    {
+        [Route(nameof(CheckHealth))]
+        public IActionResult CheckHealth()
+        {
+            if (new Random().Next(100) > 50)
+            {
+                return Ok("OK");
+            }
+            else
+            {
+                return BadRequest();
+            }
+        }
+    }
+}
